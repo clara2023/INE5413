330ba18b71a44b25f9bb03ac3f1335bc9478acd0
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2023-10-15 21:38:55 -0300

    Trabalho 1

---
+++ grafo.py
@@ -0,0 +1,238 @@
+import numpy as np
+import queue
+
+class Grafo:
+    # Implementation details
+    def __init__(self, arquivo: str) -> None:
+        # Leitura do arquivo
+        with open(arquivo) as file:
+            # *vertices n
+            self.vertices = int(file.readline().split()[1])
+            total_cells = (self.vertices**2-self.vertices)//2
+            self.grafo = np.ones((total_cells,))*np.inf
+            self.graus = np.zeros(self.vertices, dtype=int)
+            self.rotulos = np.empty(self.vertices, dtype=object)
+            # n rotulo_n
+            for _ in range(self.vertices):
+                vertice, *rotulo = file.readline().split()
+                rotulo = ' '.join(rotulo)[1:-1]
+                self.rotulos[int(vertice)-1] = rotulo
+
+            # *edges
+            file.readline()
+            self.arestas = 0
+            # a b peso
+            while line := file.readline().strip():
+                self.arestas += 1
+                a, b, peso = line.split()
+                a, b, peso = int(a), int(b), peso
+                if self[a-1, b-1] == np.inf: 
+                    self[a-1, b-1] = peso
+                    self.graus[a-1] += 1
+                    self.graus[b-1] += 1
+                else:
+                    print(f"Aresta ({a}, {b}) já existe!")
+
+    def __getitem__(self, key: int) -> float:
+        i, j = key
+        if j >= self.vertices or i >= self.vertices: raise IndexError("Index out of range")
+        
+        if i < j: return self.grafo[int(self.vertices*i + j - i*(i+1)//2 - i - 1)]
+        if i > j: return self[j, i]
+        if i == j: return 0
+
+    def __setitem__(self, key: int, value: int) -> None:
+        i, j = key
+        if j >= self.vertices or i >= self.vertices: raise IndexError("Index out of range")
+        
+        if i < j: 
+            pos = self.vertices*i + j - i*(i+1)//2 - i - 1
+            self.grafo[pos] = value
+        if i > j: self[j, i] = value
+
+    # Vizualizing
+    def mostrar_grafo(self) -> None:
+        values = [x for x in self.grafo if x != np.inf]
+        max_value_size = int(np.ceil(np.log10(max(values))))+2
+        print(("T"+"-"*(max_value_size+1))*(self.vertices)+"T")
+        for i in range(self.vertices):
+            for j in range(self.vertices):
+                coisa = self[i, j] if self[i, j] != np.inf else "∞"
+                print(f"|{coisa:^8}", end="")
+            print("|")
+            if i == self.vertices-1:
+                print(("⊥"+"-"*(max_value_size+1))*(self.vertices)+"⊥")
+            else:
+                print(("|"+"-"*(max_value_size+1))*(self.vertices)+"|")
+    
+    def grafo_completo(self) -> np.ndarray:
+        distances = np.ones((self.vertices, self.vertices))*np.inf
+        for i in range(self.vertices):
+            for j in range(self.vertices):
+                distances[i, j] = self[i, j]
+        return distances
+
+    # Questão 1
+    def qtdVertices(self) -> int:
+        return self.vertices
+    
+    def qtdArestas(self) -> int:
+        return self.arestas
+
+    def grau(self, v: int) -> int:
+        return self.graus[v]
+
+    def rotulo(self, v: int) -> str: 
+        return self.rotulos[v]
+
+    def vizinhos(self, v: int) -> list[str]:
+        vizinhos = []
+        for i in range(self.vertices):
+            if self[v, i] != np.inf and i != v:
+                vizinhos.append(self.rotulo(i))
+        return vizinhos
+
+    def haAresta(self, u: int, v: int) -> bool:
+        return self[u, v] != np.inf
+    
+    def peso(self, u: int, v: int) -> float:
+        return self[u, v]
+
+    def resposta_1(self):
+        self.mostrar_grafo()
+        print(f"Vertices: {self.qtdVertices()}")
+        print(f"Arestas: {self.qtdArestas()}")  
+        for i in range(self.vertices):
+            print(f"Grau({self.rotulo(i)}): {self.grau(i)}")
+        for i in range(self.vertices):
+            print(f"Vizinhos({self.rotulo(i)}): {self.vizinhos(i)}")
+
+    # Questão 2
+    def busca_em_largura(self, s: int) -> (np.ndarray, np.ndarray):
+        visitados = np.zeros(self.vertices, dtype=bool)
+        fila = queue.Queue()
+        fila.put(s)
+        visitados[s] = 1
+        distancias = np.zeros(self.vertices)
+        arvore = np.ones(self.vertices, dtype=int)*-1
+        while not fila.empty():
+            atual = fila.get()
+            for i in range(self.vertices):
+                if self[atual, i] != np.inf and not visitados[i]:
+                    fila.put(i)
+                    visitados[i] = 1
+                    arvore[i] = atual
+                    distancias[i] = distancias[atual] + 1
+        return (arvore, distancias)
+
+    # Questão 3
+    def ciclo_euleriano(self):
+        grafo_copy = self.grafo_completo()
+        ciclo_euleriano = []
+
+        # Escolhe um vértice para começar, de preferência um de grau ímpar ou diferente de 0
+        ponto_partida = -1
+        for i in range(self.vertices):
+            if self.grau(i) % 2 != 0:
+                ponto_partida = i
+                break
+        else:
+            for i in range(self.vertices):
+                if self.grau(i) != 0:
+                    ponto_partida = i
+                    break
+            else:
+                return []  # Não há arestas no grafo
+
+        # Comece a construir o ciclo Euleriano a partir do ponto de partida
+        v = ponto_partida
+        ciclo_euleriano.append(v)
+        while True:
+            for i in range(self.vertices):
+                if grafo_copy[v, i] != np.inf and i != v:
+                    aresta_valida = i
+                    break
+            else:
+                break
+            grafo_copy[v, aresta_valida] = np.inf
+            grafo_copy[aresta_valida, v] = np.inf
+            v = aresta_valida
+            ciclo_euleriano.append(v)
+
+        # Verifica se o caminho euleriano encontrado é ciclo
+        if ciclo_euleriano[0] != ciclo_euleriano[-1] or (len(ciclo_euleriano) == 1 and self.grau(ciclo_euleriano[0]) != 0):
+            return None
+
+        # Verifique se todas as arestas foram percorridas
+        for i in range(self.vertices):
+            for j in range(self.vertices):
+                if grafo_copy[i, j] != np.inf and i != j:
+                    return None
+        return ciclo_euleriano
+
+    # Questão 4
+    def check_for_negatives(self) -> bool:
+        return any(self[i, j] < 0 for i in range(self.vertices) for j in range(i, self.vertices))
+    
+    def bellman_ford(self, s: int, save_path: bool=True) -> (np.ndarray, np.ndarray) or np.ndarray:
+        distancias = np.ones(self.vertices)*np.inf
+        distancias[s] = 0
+        origem = np.ones(self.vertices)*-1
+        for _ in range(self.vertices - 1):
+            # Relaxar aresta (u, v)
+            for u in range(self.vertices):
+                for v in range(self.vertices):
+                    if self[u, v] and distancias[v] > distancias[u] + self[u, v]:
+                        distancias[v] = distancias[u] + self[u, v]
+                        origem[v] = u
+        for _ in range(self.vertices - 1):
+            # Relaxar aresta (u, v)
+            for u in range(self.vertices):
+                for v in range(self.vertices):
+                    if self[u, v] != np.inf and distancias[u] != -np.inf:
+                        if distancias[v] > distancias[u] + self[u, v]:
+                            distancias[v] = -np.inf
+
+        return (distancias, origem) if save_path else distancias
+    
+    def dijkstra(self, s: int, save_path: bool=True) -> (np.ndarray, np.ndarray) or np.ndarray:
+        if self.check_for_negatives(): raise Exception("O grafo possui arestas negativas")
+        distancias = np.ones(self.vertices)*np.inf
+        distancias[s] = 0
+        fila = queue.PriorityQueue()
+        fila.put((0, s))
+        origem = np.ones(self.vertices, dtype=int)*-1
+        while not fila.empty():
+            dist, u = fila.get()
+            if dist > distancias[u]: continue
+            for i in range(self.vertices):
+                if distancias[i] > distancias[u] + self[u, i]:
+                    distancias[i] = distancias[u] + self[u, i]
+                    origem[i] = u
+                    fila.put((distancias[i], i))
+        return (distancias, origem) if save_path else distancias
+
+    # Questão 5
+    def floyd_warshall(self, save_path=True) -> (np.ndarray, np.ndarray):
+        distances = self.grafo_completo()
+        origem = np.ones((self.vertices, self.vertices), dtype=int)*-1
+        for i in range(self.vertices):
+            for j in range(self.vertices):
+                if distances[i, j] != np.inf:
+                    origem[i, j] = i
+
+        for k in range(self.vertices):
+            for u in range(self.vertices):
+                for v in range(self.vertices):
+                    if distances[u, v] > distances[u, k] + distances[k, v]:
+                        distances[u, v] = distances[u, k] + distances[k, v]
+                        origem[u, v] = k
+
+        for k in range(self.vertices):
+            for u in range(self.vertices):
+                for v in range(self.vertices):
+                    if self[u, v] != np.inf and distances[u, v] != np.inf:
+                        if distances[u, v] > distances[u, k] + distances[k, v]:
+                            distances[u, v] = -np.inf
+        
+        return (distances, origem) if save_path else distances
---
+++ trabalho1.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+def resposta_5(grafo):
+    distancias, origem = grafo.floyd_warshall()
+    for i in range(grafo.vertices):
+        string = f"{int(i+1)}:"
+        for j in range(grafo.vertices):
+            if distancias[i, j] == np.inf:
+                string += "+∞,"
+            elif distancias[i, j] == -np.inf:
+                string += "-∞,"
+            else:
+                string += f"{int(distancias[i, j])},"
+        print(string[:-1])
+
+def resposta_4(grafo, s: int, alg: str) -> None:
+    if alg[0] in 'Bb': distancias, origem = grafo.bellman_ford(s)
+    elif alg[0] in 'Dd': distancias, origem = grafo.dijkstra(s)
+    else: raise Exception("Algoritmo inválido")
+    
+    for vertice in range(grafo.vertices):
+        string = f"{grafo.rotulo(vertice)}: "
+        caminho = []
+        if distancias[vertice] == np.inf:
+            string += "Não há caminho; "
+        elif distancias[vertice] == -np.inf:
+            string += "Há ciclo negativo; "
+        else:
+            atual = vertice
+            while True:
+                caminho.insert(0, atual)
+                if atual == s:
+                    break
+                atual = int(origem[atual])
+            if caminho == []:
+                string += ';'
+            else:
+                for x in caminho:
+                    string += f"{grafo.rotulo(x)},"
+                string = string[:-1] + '; '
+        string += f"d={distancias[vertice]}"
+        print(string)
+
+def resposta_3(grafo):
+    ciclo = grafo.ciclo_euleriano()
+    if ciclo is None:
+        print("0")
+    else:
+        print("1")
+        string = ','.join([str(x+1) for x in ciclo])
+        print(string if string else "O grafo não possui arestas")
+
+def resposta_2(grafo, s: int) -> (np.ndarray, np.ndarray):
+    arvore, distancias = grafo.busca_em_largura(s)
+    for i in range(int(max(distancias)+1)):
+        string = f"{i}:"
+        for j in range(grafo.vertices):
+            if distancias[int(j)] == i:
+                string += f" {j}"
+        print(string)
