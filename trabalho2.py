dd83dab3cd477f1dc94737e6a209f65c2c57d538
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2023-10-23 22:43:27 -0300

    Output questão 1 trabalho 2

--- trabalho2.py
+++ trabalho2.py
@@ -1,33 +1,21 @@
-import numpy as np
-from funcoes_auxiliares import *
 from grafo import *
 
 def resposta_1(grafo):
     predecessors = grafo.DFS_CFC()
-    S = []
-    for i in range(a.vertices):
-        S.append([i])
-    cfc = grafo.DFS_CFC()
-    componentes = np.unique(cfc)
-    
-    for componente in componentes:
-        for i, vertice in enumerate(cfc):
-            if vertice == componente: print(grafo.rotulos(i), end=',')
-        print()
-    # for v in range(a.vertices):
-    #     if predecessors[v] != -1:
-    #         print('S[v]: ', S[v])
-    #         print('S[predecessors[v]]: ', S[predecessors[v]])
-    #         a = S[v] + S[predecessors[v]]
-    #         for i in range(len(S[predecessors[v]])):
-    #             S[predecessors[v]][i] = a
-    print(S)
+    S = [[i] for i in range(grafo.qtdVertices())]
 
+    for v in range(grafo.qtdVertices()):
+        if predecessors[v] != -1:
+            a = [y for x in [S[v], S[predecessors[v]]] for y in x] 
+            for i in a:
+                S[i] = a
+    output = []
+    for row in S:
+        if row not in output:
+            output.append(row)
 
+    for i in range(len(output)):
+        print(','.join(map(grafo.rotulo, output[i])))
 
-a = GrafoDirigido("grafo.txt")
-#a.mostra_grafo()
-#print(a.DFS_CFC())
-#a.DFS_OT()
-#a.Kruskal()
-a.resposta_1()
+grafo = GrafoDirigido("grafo.txt")
+resposta_1(grafo)
