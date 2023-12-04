ac69e197c008351708ac495af366ab02dbb92d08
clara2023 <clararosa2023@gmail.com>
2023-12-04 14:30:38 -0300

    alteracoes na funcao de print

--- trabalho3.py
+++ trabalho3.py
@@ -2,17 +2,29 @@ from grafo import *
 
 def resposta_1(grafo, s, t):
     valor = grafo.edmonds_karp(s, t)
-    print(valor)
+    print("Maximum flow: ", valor)
 
 def resposta_2():
     grafo = GrafoBipartido("grafo.txt")
-    X = [0, 1, 2, 3]
-    #X = print(input("Lista de vertices no conjunto X: "))
+    X = []
+    escolha = input("Deseja selecionar os vertices do conjunto X? (s/n): ").upper()
+    if escolha == "N":
+        n = grafo.qtdVertices() // 2
+        X = [i for i in range(n)]
+    else:
+        X = [int(i)-1 for i in input("Digite os vertices do conjunto X separados por espaco. Não comece do índice 0: ").split()]
     grafo.bipartir(X)
     m, mate = grafo.hopcroft_karp()
     print("m: ", m)
-    print("Mate: ", mate)
-
+    printed = {}
+    for i in range(len(mate)):
+        a = grafo.rotulo(i)
+        b = grafo.rotulo(mate[i])
+        printed[b] = a
+        if a not in printed:
+            print("Mate:", a, ",", b)
+    # for i in range(len(mate)):
+    #     print("Mate:", grafo.rotulo(i), ",", grafo.rotulo(mate[i])) 
 def resposta_3():
     grafo = GrafoNaoDirigido("grafo.txt")
     a = grafo.lawler()
@@ -20,5 +32,5 @@ def resposta_3():
 
 grafo = GrafoDirigido("grafo.txt")
 #resposta_1(grafo, 0, 5) # 0, n-1 vertices
-#resposta_2()
-resposta_3()
+resposta_2()
+#resposta_3()
